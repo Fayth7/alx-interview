dda24d17d9f9429b6cfbe510969f690c3c30a2c6
Fayth7 <flexflakes@gmail.com>
2023-11-30 19:32:04 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+    Pascal's Triangle.
+'''
+
+def pascal_triangle(n):
+     '''
+        Function to draw Pascal's Triangle.
+    '''
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+
+def print_triangle(triangle):
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
