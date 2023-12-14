d2afccbd72dd7117f0ee75674cd60c08eb9a598e
Fayth7 <flexflakes@gmail.com>
2023-12-14 15:50:46 +0000

    task corrections

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-
-''' 
+'''
     A module that returns the minimum Operations it takes to
     get to n characters.
 '''
@@ -15,16 +13,17 @@ def minOperations(n):
     dp[1] = 0
 
     for i in range(2, n + 1):
-        for j in range(2, i + 1):
+        for j in range(1, i):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + i // j)
-    
+
     return dp[n] if dp[n] != float('inf') else 0
+
 
 # Example usage
 if __name__ == "__main__":
     n1 = 4
-    print(f"Min number of operations to reach {n1} characters: {minOperations(n1)}")
+    print(f"Min num of operations to reach {n1} chars: {minOperations(n1)}")
 
     n2 = 12
-    print(f"Min number of operations to reach {n2} characters: {minOperations(n2)}")
+    print(f"Min numb of operations to reach {n2} chars: {minOperations(n2)}")
