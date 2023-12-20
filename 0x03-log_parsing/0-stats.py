68ad7381d671a1d822daf7fd00be6965b724d801
Fayth7 <flexflakes@gmail.com>
2023-12-20 17:17:09 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,51 @@
 #!/usr/bin/python3
 """Log Parser"""
-
-
 import sys
-from collections import defaultdict
-
-def print_stats(total_size, status_counts):
-    print("File size: {}".format(total_size))
-    for status_code in sorted(status_counts.keys()):
-        count = status_counts[status_code]
-        print("{}: {}".format(status_code, count))
-
-def parse_line(line):
-    try:
-        parts = line.split()
-        ip_address = parts[0]
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
-        return ip_address, status_code, file_size
-    except (ValueError, IndexError):
-        return None, None, None
-
-def main():
-    total_size = 0
-    status_counts = defaultdict(int)
-    try:
-        for i, line in enumerate(sys.stdin, start=1):
-            ip_address, status_code, file_size = parse_line(line.strip())
-            if ip_address is not None:
-                total_size += file_size
-                status_counts[status_code] += 1
-
-            if i % 10 == 0:
-                print_stats(total_size, status_counts)
-
-    except KeyboardInterrupt:
-        print_stats(total_size, status_counts)
 
 
-if __name__ == "__main__":
-    main()
+class LogParser:
+    def __init__(self):
+        self.file_size = 0
+        self.status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                             403: 0, 404: 0, 405: 0, 500: 0}
+
+    def print_stats(self):
+        """ Print statistics """
+        print('File size: {}'.format(self.file_size))
+        for key in sorted(self.status_codes.keys()):
+            if self.status_codes[key]:
+                print('{}: {}'.format(key, self.status_codes[key]))
+
+    def parse_line(self, line):
+        """ Checks the line for matches """
+        try:
+            line = line.rstrip()
+            words = line.split(' ')
+            # File size is the last parameter on stdout
+            self.file_size += int(words[-1])
+            # Status code comes before file size
+            status_code = int(words[-2])
+            # Move through the dictionary of status codes
+            if status_code in self.status_codes:
+                self.status_codes[status_code] += 1
+        except (ValueError, IndexError):
+            pass
+
+    def process_logs(self):
+        linenum = 1
+        try:
+            for line in sys.stdin:
+                self.parse_line(line)
+                """ print after every 10 lines """
+                if linenum % 10 == 0:
+                    self.print_stats()
+                linenum += 1
+        except KeyboardInterrupt:
+            self.print_stats()
+            raise
+        self.print_stats()
+
+
+if __name__ == '__main__':
+    log_parser = LogParser()
+    log_parser.process_logs()
