9920a0042330709edf549974c4ee11b9f8ded6fe
Fayth7 <flexflakes@gmail.com>
2024-01-30 07:40:11 -0800

    changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,19 +7,22 @@ def makeChange(coins, total):
      """Determines the fewest number of coins needed to meet a given
     amount total
     """
-    if total <= 0:
+     temp_value = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
         return 0
-    rem = total
-    coins_count = 0
-    coin_idx = 0
-    sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while rem > 0:
-        if coin_idx >= n:
-            return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
-            coins_count += 1
-        else:
-            coin_idx += 1
-    return coins_count
+
+    for coin in coins:
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
+
+    return temp_value if total == 0 else -1
+
+
+if __name__ == '__main__':
+
+    print(makeChange([1, 2, 25], 37))
+
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
