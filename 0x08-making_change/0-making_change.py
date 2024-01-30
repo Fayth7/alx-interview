ac7f7c58026ada5c8819f12b434465d354386e9a
Fayth7 <flexflakes@gmail.com>
2024-01-29 20:45:35 -0800

    updates

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,25 +5,21 @@
 
 def makeChange(coins, total):
      """Determines the fewest number of coins needed to meet a given
-    amount total when given a pile of coins of different values.
+    amount total
     """
     if total <= 0:
         return 0
-
-    # Initialize an array to store the minimum number of coins for each value from 0 to total
-    dp = [float('inf')] * (total + 1)
-
-    # There are 0 coins needed to make change for 0
-    dp[0] = 0
-
-    # Iterate through each coin and update the dp array
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    # If the value at dp[total] is still infinity, it means it's not possible to make change
-    return dp[total] if dp[total] != float('inf') else -1
-
-# Test cases
-print(makeChange([1, 2, 25], 37))  # Output: 7
-print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output: -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
